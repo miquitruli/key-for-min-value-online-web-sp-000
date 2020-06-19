c23f8a969d340aff229efb0e3ce72f769e5284f4
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-19 00:03:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest = []
   name_hash.map do |key,value|
     smallest= value.min.to_i
     if name_hash(smallest)==true
