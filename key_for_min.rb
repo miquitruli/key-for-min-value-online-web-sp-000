a3b0f4caf80b4079224dc83a7a604c812c629e21
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-19 00:05:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.map do |key,value|
-    smallest= value.min.to_i
+  name_hash.map do |a,b|
+    smallest= b.min.to_i
       if name_hash(smallest)==true
-        puts "#{key}"
+        puts "#{a}"
       end
   end
 end
