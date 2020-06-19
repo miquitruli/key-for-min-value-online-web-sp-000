65cacb1a8a1ead2c614d8513ed8afae7f6bc3097
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-19 00:19:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,3 +10,16 @@ def key_for_min_value(name_hash)
   end
   empty_hash=nil
 end
+
+def key_for_min_value(name_hash)
+  empty_hash=nil
+  i=0
+  name_hash.collect do |name, value|
+    age=value.to_i
+    when age == i 
+      puts "#{name}"
+      i = i+1
+    end
+  end
+end
+  
