c22102da2a4b7675ae793cf3ddab2d5473e3b200
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-19 00:20:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,15 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  name_hash.map do |a,b|
-    smallest= b.min.to_i
-      if name_hash(smallest)==true
-        puts "#{a}"
-      end
-  end
-  empty_hash=nil
-end
 
 def key_for_min_value(name_hash)
   empty_hash=nil
